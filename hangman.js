3182e481ab8e5080388860cf688411f02eda435f
SpencerjPetty <141672620+SpencerjPetty@users.noreply.github.com>
2024-11-15 11:45:56 -0700

    Improved hangman challenge

--- hangman.js
+++ hangman.js
@@ -1,9 +1,20 @@
-//Spencer Petty 11/13/2023
-//BYU OIT coding challenge
+/*
+Improvements made since last time (~2.5 hours of extra work)
+
+1. Created an option to receive a random word from an API,
+rather than from the word bank 
+2. Made it so the random word is a certain number of letters which the user chooses
+3. Modified the Play Again functionality to be in a button rather than an alert
+4. Added the Hangman illustration and a limited number of guesses, so the player can lose
+5. Improved some QoL changes (Enter key now working for input, focus goes to the input box automatically, box is cleared after invalid inputs)
+6. Added a Quit button that redirects to Google rather than a confirm box which did the same thing
+7. Added more visual features such as coloring and the background image
+8. Full word shows after losing
+*/
 
 //This is a simple hangman game that displays in the web browser
 
-//Here is the bank of possible words to guess
+//Here is the native bank of possible words to guess
 const possibleWords = ["Candy", "Peanut", "Celebrate", "Circus", 
 "Astonishment", "Elephant", "Congratulations", 
 "JavaScript", "Coding", "Challenge"]; 
@@ -16,50 +27,87 @@ const wrongGuesses = []; //This is the bank of already guessed letters that were
 
 let numCorrectGuesses = 0; //These keep track of the number of correct and incorrect guesses
 let numIncorrectGuesses = 0;
+let lost = false;
 
-function gameStart() { //This is the function that starts/restarts the game for the user
+// Making it possible for the user to hit enter instead of clicking on the guess button
+window.onload=function(){
+    let pInput = document.getElementById("playerInput");
+
+pInput.addEventListener('keypress', function (e) { //Adding an event listener
+    if (e.key === 'Enter') {
+      playerGuess();
+        }
+    });
+  };
+
+function resetGame() {
     //These next 5 lines clear out the display for when the game is restarted by the user
     document.getElementById("numOfGuesses").innerHTML = ""; 
     document.getElementById("correctGuesses").innerHTML = "";
     document.getElementById("incorrectGuesses").innerHTML = "";
     guessDisplay.length = 0;
     document.getElementById("mainHeader").className = "";
+    document.getElementById('hangImg').src = 'hangmanDrawing/h0.png';
 
     //clears out the wrong guesses array and the guess numbers
     wrongGuesses.length = 0;
     numCorrectGuesses = 0;
     numIncorrectGuesses = 0;
-
-    //Here the word is chosen and stored in a variable
-    magicWord = possibleWords[Math.floor(Math.random() * 10)].toLowerCase();
-
-    //Here the word is turned into an array
-    magicWord = Array.from(magicWord);
-
-    //This creates the blank display word that has the correct number of letters
-    for (let i = 0; i < magicWord.length; i++) {
-    guessDisplay.push("_");
 }
 
-//And here the blank display word is shown to the user
-document.getElementById("secondHeader").innerHTML = guessDisplay.join(" ");
-
-//This announces to the user the number of letters and shows that the game has begun
-document.getElementById("mainHeader").innerHTML = `The secret word has ${magicWord.length} letters.`;
-
-};
-
-//Every time the player makes a guess, the program checks to see if they've won yet.
-
-    function playerWins() { 
-    //This is the display message that determines whether to restart the game or quit
-    if (confirm(`You won in ${(numCorrectGuesses + numIncorrectGuesses)} guesses! Would you like to play again? Press 'OK' to play again or 'cancel' to quit.`)) {
-        gameStart(); //This restarts the game if they click OK
+function gameStart(randFlag) { //This is the function that starts/restarts the game for the user
+    resetGame();
+    document.getElementById('playerInput').focus();
+    if (randFlag) { //Checks if a random word is wanted and calls the API if so
+        getNewWord().then(() => {
+            magicWord = tempWord.toString();
+            displayWord();
+        });
     } else {
-       alert("Thanks for playing!"); //Thanks the player
-       window.location.href = "http://www.google.com"; //Goes to google.com because they quit
-    };
+            //Here the word is chosen from the bank and stored in a variable
+    magicWord = possibleWords[Math.floor(Math.random() * 10)].toLowerCase();
+    displayWord();
+    }
+
 };
+// Function for displaying the empty word spaces
+function displayWord() {
+        //Here the word is turned into an array
+        magicWord = Array.from(magicWord);
+
+        //This creates the blank display word that has the correct number of letters
+        for (let i = 0; i < magicWord.length; i++) {
+        guessDisplay.push("_");
+    }
+
+    //And here the blank display word is shown to the user
+    document.getElementById("secondHeader").innerHTML = guessDisplay.join(" ");
+
+    //This announces to the user the number of letters and shows that the game has begun
+    document.getElementById("mainHeader").innerHTML = `The secret word has ${magicWord.length} letters.`;
+    };
+
+    // Function for calling the random word API
+async function getNewWord() {
+    let length = parseInt(prompt('How many letters should the random word be?')); // Asking for preferred length
+
+    if (isNaN(length) || length > 15) { // Checking validity of length chosen
+        alert('Invalid number chosen,  try again!')
+        return;
+    }
+
+    const url = `https://random-word-api.herokuapp.com/word?length=${length}`;
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+      tempWord = await response.json(); // Storing the word in a temporary variable
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
 
 //This is the function for every time the player inputs a guess
 function playerGuess() {
@@ -75,24 +123,28 @@ function playerGuess() {
     //Checking for repeat guesses
     if (wrongGuesses.includes(guess) || guessDisplay.includes(guess)) {
         alert("You've already guessed that letter!");
+        document.getElementById('playerInput').value = '';
         return;
     }
 
     //Checking for guesses of more than 1 letter
     if (guess.length > 1) {
         alert("Please input only 1 letter!");
+        document.getElementById('playerInput').value = '';
         return;
     }
     
     //Checking for guesses that aren't strings
     if (!isNaN(guess)) {
         alert("Please input a letter!");
+        document.getElementById('playerInput').value = '';
         return;
     }
 
     //Checking for blank guesses
     if (guess == "" || guess == " ") {
-        alert("You must input a guess!")
+        alert("You must input a guess!");
+        document.getElementById('playerInput').value = '';
         return;
     }
     
@@ -113,8 +165,16 @@ function playerGuess() {
     } else { //If the user guesses a letter not found in the word
         numIncorrectGuesses++ //updates incorrect guess number
         wrongGuesses.push(guess); //puts the incorrect guess in the bank
+
+        // Increments the Hangman image
+        document.getElementById('hangImg').src = `hangmanDrawing/h${numIncorrectGuesses}.png`;
         //Displays the number of incorrect guesses along with which letters have been guessed already 
-        document.getElementById("incorrectGuesses").innerHTML = "Incorrect guesses: " + numIncorrectGuesses + "<br> Letters already guessed: " + wrongGuesses.join(" ");
+        document.getElementById("incorrectGuesses").innerHTML = "Incorrect guesses: " + numIncorrectGuesses + "<br> Incorrect letters already guessed: " + wrongGuesses.join(" ");
+
+        if (numIncorrectGuesses >= 9) {
+            lost = true;
+            playerLoses();
+        }
     }
 
     //Clearing out the input box
@@ -124,13 +184,43 @@ function playerGuess() {
     document.getElementById("numOfGuesses").innerHTML = "Total guesses: " + (numCorrectGuesses + numIncorrectGuesses);
 
     //Checking if the player has guessed the entire word yet
-    if (guessDisplay.toString() === magicWord.toString()) {
-        document.getElementById("mainHeader").className = "text-rainbow-animation"; //Fun little animation
-
-        //Victory message in the HTML
-        document.getElementById("mainHeader").innerHTML = "Congratulations, you win!!! It took you " + (numCorrectGuesses + numIncorrectGuesses) + " guesses!";
-        
-        //A 2 second timeout so that the HTML has time to load before the victory analog box and you can see the animation
-        const myTimeout = setTimeout(playerWins, 2000);
+    if (guessDisplay.toString() === magicWord.toString() && !lost) {
+        playerWins();
     };
 };
+
+// Function for when the player runs out of guesses and the hangman dies
+function playerLoses() {
+
+    document.getElementById("mainHeader").innerHTML = "Aw man, you lost! Want to try again?"
+
+    // Showing the word that they missed
+    for (let i = 0; i < magicWord.length; i++) {
+            guessDisplay[i] = magicWord[i];
+            document.getElementById("secondHeader").innerHTML = guessDisplay.join(" ");
+        };
+
+    gameEnded();
+}
+
+//Every time the player makes a guess, the program checks to see if they've won yet. if so, it calls this function
+
+function playerWins() { 
+    document.getElementById("mainHeader").className = "text-rainbow-animation"; //Fun little animation
+
+    //Victory message in the HTML
+    document.getElementById("mainHeader").innerHTML = "Congratulations, you win!!! It took you " + (numCorrectGuesses + numIncorrectGuesses) + " guesses!";
+    
+    gameEnded();
+
+};
+
+function gameEnded() { // Function for hiding things when the game ends
+    document.getElementById("playAgainBtn").hidden = false;
+    document.getElementById("quitBtn").hidden = false;
+    document.getElementById("guessBtn").hidden = true
+    document.getElementById("playerInput").hidden = true;
+    document.getElementById("gameStart").hidden = true;
+    document.getElementById("randCheck").hidden = true;
+    document.getElementById("randLabel").hidden = true;
+}
